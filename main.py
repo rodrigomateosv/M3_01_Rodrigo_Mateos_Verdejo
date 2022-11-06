1c8f87c6bd587d160a956ee9eadfc924a99d82ce
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-11-06 22:18:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+
+#EJERCICIO 1 
+
+ 
+
+class Alumno:
+  def __init__(self, nombre, nota):
+    self.nombre= nombre
+    self.nota= nota
+    print("El alumno se ha creado con éxito\n")
+    print("Nombre: ", self.nombre)
+    print("Nota: ", self.nota)
+  
+  def calificacion(self):
+    if self.nota>=5:
+      print(self.nombre, "ha aprobado")
+    else:
+      print(self.nombre, "ha suspendido")
+
+
+
+alumno1=Alumno("Rodrigo", 5)
+alumno1.calificacion()
+
+print("\n\n")
+
+#Experimentamos con algunos alumnos
+Guillermo = Alumno("Guillermo", 7)
+Guillermo.calificacion()
+
+Diego = Alumno("Diego", 5)
+Diego.calificacion()
+
+Antonio = Alumno("Antonio", 2)
+Antonio.calificacion()
+
+if __name__ == "__main__":
+    main()
